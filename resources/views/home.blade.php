7d5b682b2e59ca9c0b9e7c4cac5c9a9325d6dac1
gabriellglrs <gabriellglrs@gmail.com>
2025-09-03 21:35:36 -0300

    refactor: Utiliza helpers do Laravel para assets e título
    - Substitui o título codificado por `env('APP_NAME')`.
    - Atualiza os links de CSS, JS e favicon para usar o helper `asset()` do Laravel, garantindo que as URLs sejam geradas corretamente.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>[TITLE]</title>
+    <title>{{env('APP_NAME')}}</title>
     <!-- favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png">
     <!-- bootstrap -->
-    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
     <!-- main css -->
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
 </head>
 
 <body>
@@ -109,7 +109,7 @@
 </footer>
 
 <!-- bootstrap -->
-<script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('assets/bootstrap/bootstrap.bundle.min.js') }}"></script>
 </body>
 
 </html>
